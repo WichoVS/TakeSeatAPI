240ec34bf4b8032030f79faeb9de5b3e491c82ce
Luis Villanueva <luis.villanueva.1029@gmail.com>
2022-02-27 20:24:23 -0600

    Ultimos Cambios antes de Entrega

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-//var createError = require("http-errors");
 var express = require("express");
-//var path = require("path");
-//var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
 
@@ -19,15 +16,9 @@ var usuarioRouter = require("./routes/usuario/usuario.router");
 
 var app = express();
 app.use(cors());
-// view engine setup
-//app.set("views", path.join(__dirname, "views"));
-//app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/v1/categoria", categoriaRouter);
 app.use("/api/v1/ciudad", ciudadRouter);
@@ -42,21 +33,5 @@ app.use("/api/v1/usuario", usuarioRouter);
 app.use(logsErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
-
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function (err, req, res) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
 
 module.exports = app;
